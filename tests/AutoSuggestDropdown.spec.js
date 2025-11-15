049229de8dbe4a3c64202f437bf4c0a502203dda
Samit <samithasan2000@gmail.com>
2025-11-16 02:21:07 +0600

    Commit for AutoSuggestDropdown file

---
+++ tests/AutoSuggestDropdown.spec.js
@@ -0,0 +1,25 @@
+const {test, expect} = require('@playwright/test')
+
+test('Auto suggest dropdown', async({page}) =>{
+    await page.goto('https://www.shohoz.com/')
+
+    await page.locator("//input[@id='fromcity']").fill('Dhaka') // Fill the value in the dropdown
+    await page.waitForSelector("//button/span") // waiting for the auto suggestions
+
+    const fromCityOptions = await page.$$("//button/span") // getting all the auto suggestions into a variable
+    // The X Path is not fully appropriate. 
+
+    for(let option of fromCityOptions) // reading each and every options into this option variable
+    {
+        const value = await option.textContent()
+        //console.log(value);
+        if(value.includes('Dhaka (Ashulia)'))
+        {
+            await option.click()
+            break;
+        }
+    }
+
+    await page.waitForTimeout(5000);
+
+})
